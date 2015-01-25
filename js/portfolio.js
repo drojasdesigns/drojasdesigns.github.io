e7afa095744cd9f517db820c2ca399174b4fc0d3
Trisha Tobias <trish.tobias@gmail.com>
2015-01-25 16:18:34 -0500

    Initial Commit

---
+++ js/portfolio.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+// Initial Load
+if ($(window).width() >= 699) {
+	$('#logo').fadeIn(400, function() {
+	    $('.desc').fadeIn(500);
+	    $('#social').fadeIn(500, function() {
+	    	$('.content').fadeIn(600);
+	    });
+	});
+} else {
+	$('#logo').fadeIn(500, function() {
+		$('.content').fadeIn(700);
+	});
+}
+
+// Project Name on hover
+
+	$('.red').mouseenter(function() {
+		$(this).fadeTo('fast',0.95);
+	});
+
+	$('.red').mouseleave(function() {
+		$(this).fadeTo('fast',0);
+	});
+
+});
