2e5774e32b503fce026f012dd4595900800dd9a6
Trisha <trisha@trishas-mbp.hr.cox.net>
2015-01-30 16:34:50 -0500

    Fix

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 // Mobile
 	$('.mobileexit').click(function () {
-		$('.description_container').fadeTo('fast',0, function() {
+		$('.description_container').css('display','none').fadeTo('fast',0, function() {
 			$('body').css('overflow-y','visible');
 			$('.mobileexit').fadeTo('fast',0);
 		});
