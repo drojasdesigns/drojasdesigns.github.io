680fba36804600d8ad0f44bb071ffb1ad82f12c4
Trisha <trisha@trishas-mbp.hr.cox.net>
2015-01-30 16:09:14 -0500

    Fix

--- js/portfolio.js
+++ js/portfolio.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	$('.mobileexit').click(function () {
 		$('.description_container').fadeTo('fast',0, function() {
 			if ($(window).width() > 699) {
-				($('.description_container').css('display','inline-block');
+				$('.description_container').css('display','inline-block');
 				}
 			$('body').css('overflow-y','visible');
 		});
