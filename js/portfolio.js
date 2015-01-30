22ce0224ab7af3c9d515ee5d1de7f85cc2f5ca8e
Trisha <trisha@trishas-mbp.hr.cox.net>
2015-01-30 17:25:53 -0500

    Remove display none - container

--- js/portfolio.js
+++ js/portfolio.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	});
 
 		$('.mobileexit').click(function () {
-		$('.description_container').css('display','none').fadeTo('fast',0, function() {
+		$('.description_container').fadeTo('fast',0, function() {
 			$('body').css('overflow-y','visible');
 			$('.mobileexit').fadeTo('fast',0);
 			$('.mobiletab').css('z-index','800');
