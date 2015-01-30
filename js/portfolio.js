a01c3a664967b59645ce2cd5109f19dc3fdfe852
Trisha <trisha@trishas-mbp.hr.cox.net>
2015-01-30 17:00:22 -0500

    zindex

--- js/portfolio.js
+++ js/portfolio.js
@@ -18,11 +18,11 @@ $(document).ready(function() {
 	});
 
 		$('.mobileexit').click(function () {
-		$('.description_container').css('display','none','z-index','0').fadeTo('fast',0, function() {
+		$('.description_container').css('display','none',).fadeTo('fast',0, function() {
 			$('body').css('overflow-y','visible');
 			$('.mobileexit').fadeTo('fast',0);
 			$('.mobiletab').css('z-index',1000);
-			$('#logo').css('z-index',1000);
+			$('#logo').css('z-index',1300);
 		});
 	});
 
